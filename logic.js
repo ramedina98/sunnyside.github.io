9126d878b3c7ba92bbfa952234bcd61e8984b8ca
ramedina98 <111025121+ramedina98@users.noreply.github.com>
2022-09-11 20:13:06 -0500

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,13 @@
+//This firts funtion is to open the hamburguer menu
+
+function opne(){
+
+      document.getElementById('n').style.display = 'flex';
+      document.getElementById('c').style.opacity = '0';
+}
+
+function cerrar(){
+
+      document.getElementById('n').style.display = 'none';
+      document.getElementById('c').style.opacity = '1';
+}
